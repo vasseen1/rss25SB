18b0e123cccfbf5dc2fcbf54254b7d4dafb5e25b
vasseen1 <vasse.enzo27@gmail.com>
2025-05-21 01:42:35 +0200

    start html endpoints

--- src/main/java/fr/univrouen/rss25SB/services/HtmlService.java
+++ src/main/java/fr/univrouen/rss25SB/services/HtmlService.java
@@ -1,13 +1,25 @@
 package fr.univrouen.rss25SB.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import fr.univrouen.rss25SB.entities.Item;
+import fr.univrouen.rss25SB.repositories.ItemRepository;
+
 public class HtmlService {
 
-    public void resume() {
+    private final ItemRepository itemRepository;
 
+    public HtmlService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
     }
 
-    public void resumeById(int id) {
-        
+    public List<Item> resume() {
+        return itemRepository.findAll();
+    }
+
+    public Optional<Item> resumeById(long id) {
+        return itemRepository.findById(id);
     }
     
 }
