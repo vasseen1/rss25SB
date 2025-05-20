8b83a6969925fb75cfcd380015624ca208ca454c
vasseen1 <vasse.enzo27@gmail.com>
2025-05-20 19:16:15 +0200

    modified PostService

--- src/main/java/fr/univrouen/rss25SB/repositories/ItemRepository.java
+++ src/main/java/fr/univrouen/rss25SB/repositories/ItemRepository.java
@@ -4,11 +4,8 @@ import fr.univrouen.rss25SB.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.OffsetDateTime;
-import java.util.UUID;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    // Méthode pour trouver un item par son GUID (UUID)
-    Item findByGuid(UUID guid);
     boolean existsByTitleAndPublished(String title, OffsetDateTime published);
     boolean existsByTitle(String title);
 
