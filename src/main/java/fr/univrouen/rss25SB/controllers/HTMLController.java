01ffdda17aafe45c1803e4be196652b7341a1630
vasseen1 <vasse.enzo27@gmail.com>
2025-05-21 01:51:36 +0200

    start html endpoints

--- src/main/java/fr/univrouen/rss25SB/controllers/HTMLController.java
+++ src/main/java/fr/univrouen/rss25SB/controllers/HTMLController.java
@@ -1,32 +1,42 @@
 package fr.univrouen.rss25SB.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.univrouen.rss25SB.entities.Item;
 import fr.univrouen.rss25SB.services.HtmlService;
 
 
 @RestController
 public class HTMLController {
 
-    private final HtmlService HtmlService;
+    private final HtmlService htmlService;
 
     public HTMLController(HtmlService HtmlService) {
-        this.HtmlService = HtmlService;
+        this.htmlService = HtmlService;
     }
 
     
 
+    // Récupère tous les articles
     @GetMapping("/rss25SB/resume/html")
-    public String resume() {
-        HtmlService.resume();
-        return "ok";
+    public List<Item> resume() {
+        return htmlService.resume();
     }
+
+    // Récupère un article par ID
     @GetMapping("/rss25SB/html/{id}")
-    public String id(@PathVariable int id) {
-        HtmlService.resumeById(id);
-        return "L'id récupéré est : " + id;
+    public Object resumeById(@PathVariable int id) {
+        Optional<Item> item = htmlService.resumeById((long) id);
+        if (item.isPresent()) {
+            return item.get();
+        } else {
+            return "Article non trouvé pour l'id : " + id;
+        }
     }
     
 }
