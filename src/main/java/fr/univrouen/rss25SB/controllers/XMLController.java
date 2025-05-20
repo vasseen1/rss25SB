cfdc50fe18010aa078db564dcc31fdfa637d0f36
vasseen1 <vasse.enzo27@gmail.com>
2025-05-20 14:47:33 +0200

    resume xml by id and resume all xml

--- src/main/java/fr/univrouen/rss25SB/controllers/XMLController.java
+++ src/main/java/fr/univrouen/rss25SB/controllers/XMLController.java
@@ -1,26 +1,38 @@
 package fr.univrouen.rss25SB.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import fr.univrouen.rss25SB.services.XmlService;
 
+import fr.univrouen.rss25SB.services.XmlService;
+import fr.univrouen.rss25SB.entities.Item;
 
 @RestController
 public class XMLController {
 
-    private XmlService xmlService = new XmlService();
-    
-    
-    @GetMapping("/rss25SB/resume/xml")
-    public String resume() {
-        xmlService.resume();
-        return "ok";
+    private final XmlService xmlService;
+
+    public XMLController(XmlService xmlService) {
+        this.xmlService = xmlService;
     }
 
+    // Récupère tous les articles
+    @GetMapping("/rss25SB/resume/xml")
+    public List<Item> resume() {
+        return xmlService.resume();
+    }
+
+    // Récupère un article par ID
     @GetMapping("/rss25SB/xml/{id}")
-    public String id(@PathVariable int id) {
-        xmlService.resumeById(id);
-        return "L'id récupéré est : " + id;
+    public Object resumeById(@PathVariable int id) {
+        Optional<Item> item = xmlService.resumeById((long) id);
+        if (item.isPresent()) {
+            return item.get();
+        } else {
+            return "Article non trouvé pour l'id : " + id;
+        }
     }
 }
--- src/main/java/fr/univrouen/rss25SB/services/XmlService.java
+++ src/main/java/fr/univrouen/rss25SB/services/XmlService.java
@@ -1,13 +1,27 @@
 package fr.univrouen.rss25SB.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import fr.univrouen.rss25SB.entities.Item;
+import fr.univrouen.rss25SB.repositories.ItemRepository;
+
+@Service
 public class XmlService {
 
-    public void resume() {
+    private final ItemRepository itemRepository;
 
+    public XmlService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
     }
 
-    public void resumeById(int id) {
-        
+    public List<Item> resume() {
+        return itemRepository.findAll();
     }
-    
+
+    public Optional<Item> resumeById(long id) {
+        return itemRepository.findById(id);
+    }
 }
